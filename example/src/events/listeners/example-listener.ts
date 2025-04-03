f9e7da0f57afa507515f6cd9aae7bbc4d0b14055
Mike Bates <mike@bates-solutions.com>
2025-04-02 21:51:06 -0700

    fix: 🐛 clean up example bugs

--- example/src/events/listeners/example-listener.ts
+++ example/src/events/listeners/example-listener.ts
@@ -1,4 +1,24 @@
-import { Listener } from '@bates-solutions/common';
-import { Status } from '@bates-solutions/common-example';
-
+import {} from '@bates-solutions/common';
+import {
+  Listener,
+  Status,
+  ExampleEvent,
+  Subjects,
+} from '@bates-solutions/common-example';
 import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+
+export class ExampleListerner extends Listener<ExampleEvent> {
+  readonly subject = Subjects.Example;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExampleEvent['data'], msg: Message) {
+    console.log(
+      'I should do some stuff with Status.Active and Status.Deleted',
+      Status.Active,
+      Status.Deleted
+    );
+
+    msg.ack();
+  }
+}
