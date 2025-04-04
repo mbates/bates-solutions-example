dc938d045cf7a4edd302991878d0d6bbcaa7f9ae
Mike Bates <mike@bates-solutions.com>
2025-04-04 14:58:35 -0700

    feat: working example service

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,12 +1,44 @@
-import { app } from "./app";
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+import { ExampleListerner } from './events/listeners/example-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
+    throw new Error('JWT_KEY must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on('close', () => {
+      console.log('Disconnected from NATS');
+      process.exit();
+    });
+    process.on('SIGIT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new ExampleListerner(natsWrapper.client).listen();
+
+    // await mongoose.connect(process.env.MONGO_URI);
+    // console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log("Listening on port 3000");
+    console.log('Listening on port 3000');
   });
 };
 
--- example/test/setup.ts
+++ example/test/setup.ts
@@ -1,27 +1,53 @@
-import request from "supertest";
-import { app } from "../app";
+// import { MongoMemoryServer } from 'mongodb-memory-server';
+// import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+jest.mock('../nats-wrapper');
 
 let mongo: any;
 
 beforeAll(async () => {
-  process.env.JWT_KEY = "private-key";
+  process.env.JWT_KEY = 'private-key';
+  // mongo = await MongoMemoryServer.create();
+  // const mongoUri = mongo.getUri();
+
+  // await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+
+  // if (mongoose.connection.db) {
+  //   const collections = await mongoose.connection.db.collections();
+
+  //   for (let collection of collections) {
+  //     await collection.deleteMany({});
+  //   }
+  // }
+});
+
+afterAll(async () => {
+  // if (mongo) {
+  //   await mongo.stop();
+  // }
+  // await mongoose.connection.close();
 });
 
 // could be moved to setup/auth-helper.ts and imported into *.test.ts files
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
-global.signin = async () => {
-  const email = "test@test.com";
-  const password = "password";
+global.signin = () => {
+  const token = jwt.sign(
+    {
+      // id: new mongoose.Types.ObjectId().toHexString(),
+      id: 'temp',
+      email: 'test@test.com',
+    },
+    process.env.JWT_KEY!
+  );
+  const base64 = Buffer.from(JSON.stringify({ jwt: token })).toString('base64');
 
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send({ email, password })
-    .expect(201);
-
-  const cookie = response.get("Set-Cookie");
-
-  return cookie ?? [];
+  return [`session=${base64}`];
 };
