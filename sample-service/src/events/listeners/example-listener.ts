7a66d24e6145fac927a0eac2507596064d19115b
Mike Bates <mike@bates-solutions.com>
2025-04-04 19:18:04 -0700

    feat: switch to example NATS events

---
+++ sample-service/src/events/listeners/example-listener.ts
@@ -0,0 +1,24 @@
+import {} from '@bates-solutions/common';
+import {
+  Listener,
+  Status,
+  ExampleEvent,
+  Subjects,
+} from '@bates-solutions/common-example';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+
+export class ExampleListener extends Listener<ExampleEvent> {
+  readonly subject = Subjects.Example;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExampleEvent['data'], msg: Message) {
+    console.log(
+      'I should do some stuff with Status.Active and Status.Deleted',
+      Status.Active,
+      Status.Deleted
+    );
+
+    msg.ack();
+  }
+}
